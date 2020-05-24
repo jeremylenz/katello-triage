43a958112a6e3668c6ec7a48255bdc16cf04a673
Jeremy Lenz <jeremylenz@gmail.com>
2020-05-24 14:42:58 -0400

    initial

--- src/App/index.js
+++ src/App/index.js
@@ -1,30 +1,26 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Alert, AlertActionCloseButton } from '@patternfly/react-core';
 import './app.css';
 
-export default class App extends Component {
-  state = {
-    isShowing: true
-  };
-  dismissNotification = () => {
-    this.setState({ isShowing: false });
-  };
-  render() {
-    const { isShowing } = this.state;
-    return (
-      <div className="app-container">
-        {isShowing && (
-          <div className="notification-container">
-            <Alert
-              variant="success"
-              title="Setup Complete"
-              action={<AlertActionCloseButton onClose={this.dismissNotification} />}
-            >
-              You have successfully launched your patternfly starter project.
-            </Alert>
-          </div>
-        )}
-      </div>
-    );
-  }
+const App = () => {
+  const [isShowing, setShowing] = useState(true);
+  const dismissNotification = () => setShowing(false);
+
+  return (
+    <div className="app-container">
+      {isShowing && (
+        <div className="notification-container">
+          <Alert
+            variant="success"
+            title="Setup Complete"
+            action={<AlertActionCloseButton onClose={dismissNotification} />}
+          >
+            You have successfully launched your patternfly starter project.
+          </Alert>
+        </div>
+      )}
+    </div>
+  );
 }
+
+export default App;
